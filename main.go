efca54f8f255f4c480740aeca5cdb9aa6b992e35
Novian Rachman Iskandar <novian19@upi.edu>
2023-05-28 19:54:22 +0700

    fixes connstring

--- main.go
+++ main.go
@@ -19,14 +19,14 @@ var (
 func init() {
 
 	var (
-		username   = os.Getenv("DATABASE_USERNAME")
-		password   = os.Getenv("DATABASE_PASSWORD")
-		serverName = os.Getenv("DATABASE_HOST")
-		port       = os.Getenv("DATABASE_PORT")
-		database   = os.Getenv("DATABASE_NAME")
+		username = os.Getenv("DATABASE_USERNAME")
+		password = os.Getenv("DATABASE_PASSWORD")
+		host     = os.Getenv("DATABASE_HOST")
+		port     = os.Getenv("DATABASE_PORT")
+		database = os.Getenv("DATABASE_NAME")
 	)
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", username, password, serverName, port, database)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, database)
 
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
