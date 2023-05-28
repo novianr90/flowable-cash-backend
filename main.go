bc60d8b4abf0f899adb332f3551065a0c2cc2874
Novian Rachman Iskandar <novian19@upi.edu>
2023-05-28 20:01:05 +0700

    fix port on mysql

--- main.go
+++ main.go
@@ -22,11 +22,10 @@ func init() {
 		username = os.Getenv("DATABASE_USERNAME")
 		password = os.Getenv("DATABASE_PASSWORD")
 		host     = os.Getenv("DATABASE_HOST")
-		port     = os.Getenv("DATABASE_PORT")
 		database = os.Getenv("DATABASE_NAME")
 	)
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, database)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, database)
 
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
