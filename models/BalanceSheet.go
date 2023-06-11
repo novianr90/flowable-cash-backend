0e2ba07fbdfa0c84661a1a990e206896c1064f9a
Novian Rachman Iskandar <novian19@upi.edu>
2023-06-12 03:46:56 +0700

    Add hook to check if account name already exist

--- models/BalanceSheet.go
+++ models/BalanceSheet.go
@@ -1,6 +1,11 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type BalanceSheet struct {
 	ID          uint   `gorm:"primaryKey;not null"`
@@ -9,4 +14,35 @@ type BalanceSheet struct {
 	Balance     []byte `gorm:"not null"`
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
+}
+
+func (b *BalanceSheet) BeforeCreate(tx *gorm.DB) error {
+	var balanceSheet BalanceSheet
+
+	accountNameToCheck := []string{
+		"Kas",
+		"Persediaan Barang Dagang",
+		"Device",
+		"Hutang Dagang",
+		"Modal",
+		"Laba Disimpan",
+		"Mengambil Laba",
+		"Penjualan",
+		"Pembelian",
+		"Beban Pembelian",
+		"Beban Penjualan",
+	}
+
+	for _, value := range accountNameToCheck {
+		err := tx.Where("account_name = ?", value).First(&balanceSheet).Error
+
+		if err == nil {
+			return errors.New("data already filled, please delete or update")
+		}
+	}
+
+	_ = balanceSheet
+
+	return nil
+
 }
