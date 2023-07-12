b1f549e9f3c2309c48321077922b66d9df3ebc91
Novian Rachman Iskandar <novian19@upi.edu>
2023-07-13 03:07:59 +0700

    add hooks on posting

--- models/Posting.go
+++ models/Posting.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Posting struct {
 	ID            uint      `gorm:"primaryKey; not null" json:"posting_id" form:"posting_id"`
@@ -8,4 +12,18 @@ type Posting struct {
 	AccountID     uint      `gorm:"not null" json:"account_id" form:"account_id"`
 	CreatedAt     time.Time `json:"created_at"`
 	UpdatedAt     time.Time `json:"updated_at"`
+}
+
+func (p *Posting) BeforeCreate(tx *gorm.DB) error {
+	if isDuplicateID(tx, p.TransactionID) {
+		return gorm.ErrDuplicatedKey
+	}
+
+	return nil
+}
+
+func isDuplicateID(tx *gorm.DB, id uint) bool {
+	var count int64
+	tx.Model(&Posting{}).Where("transaction_id = ?", id).Count(&count)
+	return count > 0
 }
